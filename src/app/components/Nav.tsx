c7eb94459f2e8f21c9c3f76cc0eac72a798822e3
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-05-06 23:52:24 +0200

    create navbar

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+const Nav = () => {
+  return (
+    <nav className="bg-gray-200 p-3 pb-0 flex flex-wrap items-center justify-between">
+      <Image src="/CookMania.png" alt="CookMania" width={120} height={120} />
+      <div className="flex items-center h-10 pl-3 m-6 bg-gray-100 rounded-lg">
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <input
+          id="search"
+          type="text"
+          placeholder="Search recipes..."
+          className="w-full h-full pl-2 bg-transparent outline-none"
+        />
+      </div>
+      <ul className="flex space-around text-l gap-4 pr-4 ">
+        <Link href="/" className="hover:text-blue-500">
+          Log In
+        </Link>
+        <Link href="/" className="hover:text-blue-500">
+          Profile
+        </Link>
+        <Link href="/" className="hover:text-blue-500">
+          About Us
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from "./components/Nav";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
