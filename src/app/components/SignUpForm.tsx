0948509bec6d2ee971f4455def6c2586be6ffa56
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-05-20 22:16:17 +0200

    create register page

---
+++ src/app/components/SignUpForm.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+
+const FormSchema = z
+  .object({
+    username: z
+      .string()
+      .min(2, "First Name must be at least 2 characters")
+      .max(45, "First Name must be less than 45 characters")
+      .regex(new RegExp("^[a-zA-Z0-9.@_-]+$"), "This username is not valid"),
+    email: z.string().email("Please enter a valid email address"),
+
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .max(50, "Password must be at less than 50 characters"),
+    repeatPassword: z.string(),
+  })
+  .refine((data) => data.password === data.repeatPassword, {
+    message: "Password and confirm password does not match",
+    path: ["repeatPassword"],
+  });
+
+type InputType = z.infer<typeof FormSchema>;
+
+export default function SignUpForm() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputType>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const saveUser: SubmitHandler<InputType> = async (data) => {
+    console.log("test");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(saveUser)}
+      className="flex flex-col gap-2 items-center justify-center border-2 border-gray-300 p-4 rounded-md"
+    >
+      <input
+        className="border-2 rounded-sm outline-gray-300 px-2 py-1"
+        type="email"
+        placeholder="email"
+        {...register("email")}
+      />
+      {errors.email && (
+        <p className="mt-2 italic text-md text-error">
+          {errors.email?.message}
+        </p>
+      )}
+      <input
+        className="border-2 rounded-sm outline-gray-300 px-2 py-1"
+        type="text"
+        placeholder="username"
+        {...register("username")}
+      />
+      {errors.username && (
+        <p className="mt-2 italic text-md text-error">
+          {errors.username?.message}
+        </p>
+      )}
+      <input
+        className="border-2 rounded-sm outline-gray-300 px-2 py-1"
+        type="password"
+        placeholder="password"
+        {...register("password")}
+      />
+      {errors.password && (
+        <p className="mt-2 italic text-md text-error">
+          {errors.password?.message}
+        </p>
+      )}
+      <input
+        className="border-2 rounded-sm outline-gray-300 px-2 py-1"
+        type="password"
+        placeholder="repeat the password"
+        {...register("repeatPassword")}
+      />
+      {errors.repeatPassword && (
+        <p className="mt-2 italic text-md text-error">
+          {errors.repeatPassword?.message}
+        </p>
+      )}
+      <button
+        type="submit"
+        className="bg-gray-200 rounded-sm w-full p-1 hover:bg-gray-300"
+      >
+        register
+      </button>
+    </form>
+  );
+}
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
-import LoginForm from "../components/LoginForm";
+import SignUpForm from "../components/SignUpForm";
 
-export default function Login() {
+export default function Register() {
   return (
     <div className="w-11/12 mx-auto md:w-10/12 lg:w-9/12 flex flex-col items-center justify-center gap-3 text-xl">
-      <h1>Login to your account</h1>
-      <LoginForm />
+      <h1>Create a new account</h1>
+      <SignUpForm />
       <p>
-        Do not have an account yet?
+        Already have an account?
         <Link
-          href="/register"
+          href="/login"
           className=" text-gray-500 hover:text-blue-500 hover:underline hover:underline-offset-4 ml-2"
         >
-          Register
+          Login
         </Link>
       </p>
     </div>
